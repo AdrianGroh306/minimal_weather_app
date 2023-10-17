5b34750c568e3bab87a4271b5a5dda3baf2aca6b
AdrianGroh <116496298+AdrianGroh306@users.noreply.github.com>
2023-10-18 00:57:36 +0200

    Change: setting up the weather app

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,11 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_weather_app/models/weather_model.dart';
+import 'package:minimal_weather_app/service/weather_service.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService(apiKey);
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async{
+    //get curent city
+    String cityName = await _weatherService.getCurrentCity();
+
+    //get weather for city
+    try{
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    catch(e){
+      print(e);
+    }
+  }
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+
 
     );
   }
